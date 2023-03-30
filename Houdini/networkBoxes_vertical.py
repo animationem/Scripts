1a8725a1c7d6efb9833dbf097fd4e4773bd61cfb
Animationem <john.kesig@icloud.com>
2023-03-30 12:34:41 -0600

    optimized script instead of the long one

--- Houdini/networkBoxes_vertical.py
+++ Houdini/networkBoxes_vertical.py
@@ -5,42 +5,15 @@ root = hou.node('/obj/')
 # On Linux this most likely located in your home folder: /home/(user)/(houdini_version)/scripts/123.py
 # If the scripts folder isn't there, go ahead and create it. Same goes for the 123.py
 
-# Create Vertical Network Boxes, set poitions and colors
-### Create Import Network Box
-nb_import = root.createNetworkBox(name='Import')
-nb_import.setComment('Import')
-nb_import.setSize([5.0, 8.0])
-nb_import.setPosition([-10, 0])
+nw_boxes = {'Import' : (), 'Geo' : (0.6, 1, 0.25), 'Lighting' : (0.75, 0.75, 0), 'FX' : (0, 0.35, 0.75), 'Render' : (0.37, 0.205, 1)}
+pos = 10
 
-### Create Geometry Network Box
-nb_geo = root.createNetworkBox()
-nb_geo.setName('Geo')
-nb_geo.setComment('Geometry')
-nb_geo.setSize([5.0, 8.0])
-nb_geo.setColor(hou.Color(0.6, 1, 0.25))
-nb_geo.setPosition([-4, 0])
-
-### Create Lighting Network Box
-nb_lgt = root.createNetworkBox()
-nb_lgt.setName('Lighting')
-nb_lgt.setComment('Lighting')
-nb_lgt.setSize([5.0, 8.0])
-nb_lgt.setColor(hou.Color(0.75, 0.75, 0))
-nb_lgt.setPosition([2, 0])
-
-### Create FX Network Box
-nb_fx = root.createNetworkBox()
-nb_fx.setName('FX')
-nb_fx.setComment('FX')
-nb_fx.setSize([5.0, 8.0])
-nb_fx.setColor(hou.Color(0, 0.35, 0.75))
-nb_fx.setPosition([8, 0])
-
-### Create Render Network Box
-nb_ren = root.createNetworkBox()
-nb_ren.setName('Render')
-nb_ren.setComment('Render')
-nb_ren.setSize([5.0, 8.0])
-nb_ren.setColor(hou.Color(0.37, 0.205, 1))
-nb_ren.setPosition([14, 0])
-
+# Create Vertical Network Boxes, set size, comment, positions, and colors
+for key, value in nw_boxes.items():
+    nb = root.createNetworkBox(name=key)
+    nb.setComment(key)
+    nb.setSize([4.0, 8.0])
+    nb.setColor(hou.Color(value))
+    nb.setPosition([0,pos])
+        
+    pos -= 5
