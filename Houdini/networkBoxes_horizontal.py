ea6873e517650a96ce18431733cf34208def53e1
Animationem <john.kesig@icloud.com>
2023-03-30 12:04:15 -0600

    Optimized the script

--- Houdini/networkBoxes_horizontal.py
+++ Houdini/networkBoxes_horizontal.py
@@ -5,9 +5,7 @@ root = hou.node('/obj/')
 # On Linux this most likely located in your home folder: /home/(user)/(houdini_version)/scripts/123.py
 # If the scripts folder isn't there, go ahead and create it. Same goes for the 123.py
 
-
 nw_boxes = {'Import' : (), 'Geo' : (0.6, 1, 0.25), 'Lighting' : (0.75, 0.75, 0), 'FX' : (0, 0.35, 0.75), 'Render' : (0.37, 0.205, 1)}
-nb_count = len(nw_boxes)
 pos = 10
 
 # Create Vertical Network Boxes, set size, comment, positions, and colors
