871e382c9c3ddb1b878434292152f04efb32c2dc
Animationem <john.kesig@icloud.com>
2023-04-07 14:41:00 -0600

    changed the code again to be cleaner and doesn't require an extra external class

--- Houdini/networkBoxes_vertical.py
+++ Houdini/networkBoxes_vertical.py
@@ -1,27 +1,23 @@
 import hou
-from collections import OrderedDict
 
 root = hou.node('/obj/')
 
-# Create the dictionary that will be ordered by first come first serve
-nw_boxes = OrderedDict()
+# Create the dictionary that will store the key and value pairs
+nw_boxes = {'ASSETS': (0.281, 0.281, 0.278), 'GEO': (0.529, 0.596, 0.415), 'LGT': (0.980, 0.898, 0.533), 'FX': (0.455, 0.580, 0.917), 'RENDER': (0.709, 0.611, 0.882)}
 
-# Create the keys and values that will be added to the dictionary variable above
-nw_boxes["Assets"] = (0.281, 0.281, 0.278)
-nw_boxes["GEO"] = (0.529, 0.596, 0.415)
-nw_boxes["LGT"] = (0.980, 0.898, 0.533)
-nw_boxes["FX"] = (0.455, 0.580, 0.917)
-nw_boxes["RENDER"] = (0.709, 0.611, 0.882)
+# Set the order by which the network boxes will be generated
+nw_order = ['ASSETS', 'GEO', 'LGT', 'FX', 'RENDER']
 
 # The network boxes will start at this position in the network
 pos = -16
 
 # Create Vertical Network Boxes, set size, comment, positions, and colors
-for key, value in nw_boxes.items():
+for key in nw_order:
+    value = nw_boxes[key]
     nb = root.createNetworkBox(name=key)
     nb.setComment(key)
     nb.setSize([4.0, 8.0])
     nb.setColor(hou.Color(value))
-    nb.setPosition([pos,0])
-        
+    nb.setPosition([pos, 0])
+
     pos += 5
